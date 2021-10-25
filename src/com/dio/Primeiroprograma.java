732c09130c8ce2a4e045d4735cafc45c2f07fe89
Cassiu.dev <cassioouvera@hotmail.com>
2021-10-24 23:00:27 -0300

    first commit

---
+++ src/com/dio/Primeiroprograma.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+		
+		/*int a =2;
+		int b =3;
+		
+		System.out.println("Hello word!" +(a+b));*/
+	}
+	
+	class Livros{
+		private String nome;
+		private String npag;
+	}
+}
